94b81fdb862f1b9a34af6e08e6f5e8a3d904d3d6
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-10-20 00:50:52 +0700

    CartScreen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -5,17 +5,17 @@ import {
   Row,
   Col,
   Button,
-  Cart,
+  Card,
   ListGroup,
   Image,
   Form,
 } from 'react-bootstrap';
 
 import { addToCart } from '../actions/cartActions';
+import Message from '../components/Message';
 
 const CartScreen = ({ match, location, history }) => {
   const productId = match.params.id;
-  console.log(productId);
 
   // location.search -> ?qty=4
   const quantity = location.search ? Number(location.search.split('=')[1]) : 1;
@@ -23,18 +23,113 @@ const CartScreen = ({ match, location, history }) => {
   const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
 
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, quantity));
     }
   }, [dispatch, productId, quantity]);
+
+  // EnventListeners
+  const removeFromCartHandler = (id) => {
+    console.log(id);
+  };
+
+  const checkoutHandler = () => {
+    history.push('/login?redirect=shipping');
+  };
+
   return (
+    // Alert
     <Row>
-      <Col md={8}></Col>
-      <h1>Shopping Cart</h1>
-      <Col md={2}></Col>
-      <Col md={2}></Col>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.lengh === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'> Go Back</Link>
+          </Message>
+        ) : (
+          // Cart Item
+          <ListGroup varient='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/products/${item.product}`}> {item.name}</Link>
+                  </Col>
+                  <Col md={2}>{item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as='select'
+                      value={item.quantity}
+                      onChange={(e) =>
+                        // Recall the action
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((index) => (
+                        <option key={index + 1} value={index + 1}>
+                          {index + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() => {
+                        removeFromCartHandler(item.product);
+                      }}
+                    >
+                      <i className='fas fa-trash'></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup>
+            <ListGroup.Item>
+              <h2>
+                Subtotal (
+                {
+                  // total number of items
+                  cartItems.reduce((acc, item) => acc + item.quantity, 0)
+                }
+                ) items
+              </h2>
+              $
+              {
+                // Total Price
+                cartItems
+                  .reduce((acc, item) => acc + item.quantity * item.price, 0)
+                  .toFixed(2)
+              }
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                className='btn-block'
+                type='button'
+                disabled={cartItems.length === 0}
+                onClick={checkoutHandler}
+              >
+                Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
     </Row>
   );
 };
