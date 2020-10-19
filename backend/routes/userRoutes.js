c0a794e3571a6e3405015c428bf6b178e1364dd2
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-10-20 04:01:45 +0700

    Custom Auth Middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
-import { urlencoded } from 'body-parser';
 import express from 'express';
 
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', authUser);
+
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
