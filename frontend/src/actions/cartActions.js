894821831c752c4fc8e6d855cc950103d1b3f2a4
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-10-23 06:45:36 +0700

    Payment Screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
+  CART_SAVE_PAYMENT_METHOD,
   CART_SAVE_SHIPPING_ADDRESS,
 } from '../constants/cartConstants';
 
@@ -42,5 +43,17 @@ export const saveShippingAddress = (data) => (dispatch, getState) => {
   localStorage.setItem(
     'shippingAddress',
     JSON.stringify(getState().cart.shippingAddress)
+  );
+};
+
+export const savePaymentMethod = (data) => (dispatch, getState) => {
+  dispatch({
+    type: CART_SAVE_PAYMENT_METHOD,
+    payload: data,
+  });
+
+  localStorage.setItem(
+    'paymentMethod',
+    JSON.stringify(getState().cart.paymentMethod)
   );
 };
