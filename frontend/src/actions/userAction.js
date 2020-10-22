344f21b9c0d9fdedd74fb4680230797e762d152e
Ilyas Ezzahid <ezzil1997@gmail.com>
2020-10-23 00:49:17 +0700

    Server: PUT api/users/profile

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -60,7 +60,7 @@ export const register = (name, email, password) => async (dispatch) => {
     };
 
     const { data } = await axios.post(
-      '/api/users/register',
+      '/api/users/',
       { name, email, password },
       config
     );
